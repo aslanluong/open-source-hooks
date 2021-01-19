0dad5bd0c19c951eec8954e0cd3a563ac08e67c9
Thang Luong <luongththang@gmail.com>
2021-01-19 16:00:43 +0700

    cicd: add test workflow

---
+++ open-source-hooks/jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  testMatch: ['<rootDir>/tests/**/*.test.(ts|tsx)'],
+};
+
+export default config;
